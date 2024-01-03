d97398d8d546a3fdc3041563c56de86d6a551f15
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-03 13:44:19 +0200

    Task 13

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - binary tree function
+ *
+ * measures the leaves of a binary tree
+ *
+ * @tree: a pointer to the root node of the tree to measure the leaves
+ *
+ * Return: leaves of a binary tree
+ * or 0 If tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+			leaves++;
+		if (tree->right != NULL)
+			leaves++;
+		if (tree->left == NULL && tree->right == NULL)
+			leaves++;
+	}
+
+	return (leaves);
+}
