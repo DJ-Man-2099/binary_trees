f226984013092faa42613e30fbd32e53d9f7f971
malakehab2003 <lokaehab2003@gmail.com>
2024-01-04 16:36:01 +0200

    fix style

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 binary_tree_t *equal_anc(binary_tree_t *f, binary_tree_t *s);
-binary_tree_t *not_equal_anc(binary_tree_t *f, binary_tree_t *s, int cf, int cs);
+binary_tree_t *not_eq_anc(binary_tree_t *f, binary_tree_t *s, int cf, int cs);
 int count_ancestor(binary_tree_t *node);
 
 /**
- * binary_trees_ancestor - find the lowest common ancestor of two nodes 
+ * binary_trees_ancestor - find the lowest common ancestor of two nodes
  *
  * Return: pointer to the lowest common ancestor or null
  *
@@ -14,7 +14,8 @@ int count_ancestor(binary_tree_t *node);
  * @second: the second node
 */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	const binary_tree_t *second)
 {
 	int anc_first, anc_second;
 	binary_tree_t *f, *s;
@@ -34,14 +35,28 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 	else
 	{
 		if (anc_first > anc_second)
-			return (not_equal_anc(f, s, anc_first, anc_second));
+			return (not_eq_anc(f, s, anc_first, anc_second));
 		else if (anc_second > anc_first)
-			return (not_equal_anc(s, f, anc_second, anc_first));
+			return (not_eq_anc(s, f, anc_second, anc_first));
 	}
 	return (NULL);
 }
 
-binary_tree_t *not_equal_anc(binary_tree_t *f, binary_tree_t *s, int cf, int cs)
+/**
+ * not_eq_anc - get common ancestor if parents not equal
+ *
+ * Return: the ancestor or NULL
+ *
+ * @f: the first point
+ *
+ * @s: the second point
+ *
+ * @cf: the count of ancestors of first point
+ *
+ * @cs: the count of ancestors of second point
+*/
+
+binary_tree_t *not_eq_anc(binary_tree_t *f, binary_tree_t *s, int cf, int cs)
 {
 	while (f->parent != NULL && cf > cs)
 	{
@@ -55,6 +70,16 @@ binary_tree_t *not_equal_anc(binary_tree_t *f, binary_tree_t *s, int cf, int cs)
 	return (NULL);
 }
 
+/**
+ * equal_anc - get common ancestor if parents equal
+ *
+ * Return: the ancestor or NULL
+ *
+ * @f: the first point
+ *
+ * @s: the second point
+*/
+
 binary_tree_t *equal_anc(binary_tree_t *f, binary_tree_t *s)
 {
 	while (f->parent || s->parent)
@@ -67,6 +92,14 @@ binary_tree_t *equal_anc(binary_tree_t *f, binary_tree_t *s)
 	return (NULL);
 }
 
+/**
+ * count_ancestor - count number of ancestors of a node in binary tree
+ *
+ * Return: the number of ancestors
+ *
+ * @node: the node to count its ancs
+*/
+
 int count_ancestor(binary_tree_t *node)
 {
 	int count = 0;
@@ -77,5 +110,5 @@ int count_ancestor(binary_tree_t *node)
 		node = node->parent;
 	}
 
-	return count;
+	return (count);
 }
