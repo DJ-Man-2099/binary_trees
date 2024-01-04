f6a2a118bf49685c8780312de89e046546d67f48
malakehab2003 <lokaehab2003@gmail.com>
2024-01-04 20:20:36 +0200

    add Level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 void run_func(void (*func)(int), binary_tree_t *root, int cur);
-
+size_t binary_tree_height(const binary_tree_t *tree);
 /**
  * binary_tree_levelorder - run function with the data of the node
  *
@@ -15,7 +15,7 @@ void run_func(void (*func)(int), binary_tree_t *root, int cur);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	binary_tree_t *root;
-	int i, height;
+	size_t height, i;
 
 	if (tree == NULL || func == NULL)
 		return;
@@ -38,4 +38,23 @@ void run_func(void (*func)(int), binary_tree_t *root, int cur)
 		run_func(func, root->left, cur - 1);
 		run_func(func, root->right, cur - 1);
 	}
+}
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        size_t height = 0, other_height = 0;
+
+        if (tree != NULL)
+        {
+                height = binary_tree_height(tree->left);
+                if (tree->left != NULL)
+                        height++;
+                other_height = binary_tree_height(tree->right);
+                if (tree->right != NULL)
+                        other_height++;
+                if (other_height > height)
+                        height = other_height;
+        }
+
+        return (height);
 }
